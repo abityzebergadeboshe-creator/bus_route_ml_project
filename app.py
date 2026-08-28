8cc0feb79f956dea4c9560f5979b1cf9e130d8f9
abityzebergadeboshe-creator <abityzebergadeboshe@gmail.com>
2026-08-28 11:24:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
-import joblib
+import joblib 
 import pandas as pd
 from datetime import datetime, timedelta
 
